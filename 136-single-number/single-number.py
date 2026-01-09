99fe6b1858243390deaeb34b816e3ae2f207ae41
equistrowned <animesh4coding@gmail.com>
2026-01-09 20:55:35 +0530

    Time: 12 ms (26.87%) | Memory: 15.4 MB (6.54%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -4,21 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # count = {}
+        count = {}
 
-        # for i in range(len(nums)):
-        #     if nums[i] not in count:
-        #         count[nums[i]] = 1
-        #     else:
-        #         count[nums[i]] += 1
+        for i in range(len(nums)):
+            if nums[i] not in count:
+                count[nums[i]] = 1
+            else:
+                count[nums[i]] += 1
 
-        # for key, value in count.items():
-        #     if value == 1:
-        #         return key
+        for key, value in count.items():
+            if value == 1:
+                return key
 
-        nums.sort()
-        for i in range(0, len(nums)-1, 2):
-            if nums[i] != nums[i+1]:
-                return nums[i]
-        return nums[-1]
+        # nums.sort()
+        # for i in range(0, len(nums)-1, 2):
+        #     if nums[i] != nums[i+1]:
+        #         return nums[i]
+        # return nums[-1]
+
+
 
