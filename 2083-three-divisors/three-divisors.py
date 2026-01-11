226c562fe9b4ca32f41c6b3e036fab84a12bd81d
equistrowned <animesh4coding@gmail.com>
2026-01-11 21:10:50 +0530

    Time: 7 ms (23.51%) | Memory: 12.9 MB (20.32%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        divisors = 0
+        flag = False
+
+        for i in range(1, n+1):
+            if n%i == 0:
+                divisors += 1
+        
+        if divisors == 3:
+            return True
+        else:
+            return False
