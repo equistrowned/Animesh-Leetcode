be21c792cd193d49a4a841a7f549c6e7867e4072
equistrowned <animesh4coding@gmail.com>
2026-01-20 23:41:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.15%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,14 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        times = len(nums)
+        #O(n^2) comp
+        # times = len(nums)
 
-        for _ in range(times):
-            l = 0
-            r = 1
+        # for _ in range(times):
+        #     l = 0
+        #     r = 1
 
-            while r < times:
-                if nums[l] > nums[r]:
-                    nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r += 1
+        #     while r < times:
+        #         if nums[l] > nums[r]:
+        #             nums[l], nums[r] = nums[r], nums[l]
+        #         l += 1
+        #         r += 1
+
+        nums2 = nums.copy()
+        nums.clear()
+
+        for i in range(len(nums2)):
+            mn = min(nums2)
+            nums.append(mn)
+            nums2.remove(mn)
+
