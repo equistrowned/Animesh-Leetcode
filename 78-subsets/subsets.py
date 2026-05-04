fce34cafc44687cd816dac8e6861fe9bd0f038e8
equistrowned <animesh4coding@gmail.com>
2026-05-04 20:33:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (28.40%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,23 +1,16 @@
-class Solution(object):
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        res, sol = [], []
+        sol, res = [], []
 
         def backtrack(i):
             if i == n:
                 res.append(sol[:])
                 return
-            
             backtrack(i+1)
-
             sol.append(nums[i])
             backtrack(i+1)
             sol.pop()
 
         backtrack(0)
         return res
-            
